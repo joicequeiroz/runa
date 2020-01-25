629b7eb8ab2b8d10f86cdbc9bae3a36e8cf313b8
joice.qs@gmail.com <joice.qs@gmail.com>
2020-01-25 17:26:44 -0300

    'update'

--- features/steps/payroll.py
+++ features/steps/payroll.py
@@ -15,11 +15,11 @@ password = 'runahr'
 element_email = 'email'
 element_password = 'password'
 button_sub = 'button.is-submit'
-element_hello = "//h1[contains(.,'¡Bienvenido, Administrador!')]"
+hello = "//h1[contains(.,'¡Bienvenido, Administrador!')]"
 payroll_menu = "//a[contains(.,'payroll')]"
 btn_new = "button.button.is-light-green"
-select_group = '.react-select__indicators'
-
+wind_payroll = "//h1[contains(.,'Nueva nómina manual')]"
+search_group = '.react-select__indicators'
 
 @given('I have access to Runa RH as admin')
 def step_impl(context):
@@ -27,7 +27,7 @@ def step_impl(context):
     EMAIL = (By.ID, element_email)
     PASS = (By.ID, element_password)
     SUBMIT = (By.CLASS_NAME, button_sub)
-    RESULT = (By.XPATH, element_hello)
+    RESULT = (By.XPATH, hello)
 
     def login(email, password):
         context.web.find_element(*EMAIL).send_keys(user)
@@ -36,45 +36,56 @@ def step_impl(context):
     login(user, password)
 
     def validate(value):
-        context.element_hello = context.web.find_element(*RESULT)
-        assert "¡Bienvenido, Administrador!" in element_hello.text
-    
+        context.hello = context.web.find_element(*RESULT)
+        assert "¡Bienvenido, Administrador!" in hello.text
+
+
 @when('I want to create a new Payroll')
 def step_impl(context):
     MENU_PAYROLL = (By.XPATH, payroll_menu)
     BTN_NEW = (By.CLASS_NAME, btn_new)
+    RES_NEW = (By.XPATH, wind_payroll)
 
     def create_payroll(MENU_PAYROLL):
         element = context.web.find_elements(*MENU_PAYROLL)
 
         for x in element:
 
-            if x.text == "nome do botao":
+            if x.text == "menu name":
                 x.click()
 
     create_payroll(MENU_PAYROLL)
 
     def new_payroll(BTN_NEW):
-        element = context.web.find_elements(*BTN_NEW)
+        element = context.web.find_element(*BTN_NEW)
 
         for x in element:
 
-            if x.text == "nome do botao":
+            if x.text == "button name":
                 x.click()
 
     create_payroll(BTN_NEW)
 
+    def validate(value):
+        context.wind_payroll = context.web.find_element(*RES_NEW)
+        assert "Nueva nómina manual" in wind_payroll.text
+
+
 @when('I find the payroll group assigned to me')
 def step_impl(context):
     #raise NotImplementedError(u'STEP: When I find the payroll group assigned to me')
-    # for key, handle in enumerate(driver.current_window_handle):
-    #     print(key, handle)
-    #     print("Janela Atual: {}".format(driver.current_window_handle))
-    driver.switch_to_window(driver.current_window_handle[1])
-    # driver.switch_to.window("_blank")
-    GROUP = (By.CSS_SELECTOR, select_group)
+    # driver.switch_to_window('_blank')
+    GROUP = (By.CSS_SELECTOR, search_group)
+    # context.web.find_element(*GROUP).click()
+
     def select_group(GROUP):
-        element = context.web.find_elements(GROUP).click()
+        element = context.web.find_element(*GROUP)
+
+        for x in element:
+            
+            if x.text == "group name":
+                x.click()
+                x.set("Grupo 1")
 
     select_group(GROUP)
 
